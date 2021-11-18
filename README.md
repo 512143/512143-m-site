78d6bd71afc41b514ddb152ef358ab6e2ec930b4
x0rzz <82827201+512143@users.noreply.github.com>
2021-11-18 19:24:13 +0300

    начал создавать страницу с входом

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# 512143-m-site
+# mon
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <div class= "parents">
+      <form class="main">
+        <div class="form-item"> 
+          <input type="text">
+          
+        </div>
+        <div class="form-item"> 
+          <input type="text">
+        </div>
+        <div class="form-item"> 
+          <button>Войти</button>
+        </div>
+      </form>
+   </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+.parents {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.main{
+  width: 430px;
+  height: 520px;
+  background-color: aqua;
+  margin-top: 150px;
+  border-radius:10px ;
+  padding:40px 60px;
+
+  
+} 
+.form-item{
+  width: 100%;
+  height: 100px;
+}
+</style>
